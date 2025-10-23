776db49d4115a3e3cb51fc4a9e7398783d4e5603
Jaime Redondo <jairegra@gmail.com>
2025-10-23 10:43:20 +0200

    Implement interrupts and input buffering

--- src/main.c
+++ src/main.c
@@ -1,7 +1,43 @@
+#include <fcntl.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+/* -------------------------------------------------------------------------- */
+/*                               INPUT BUFFERING                              */
+/* -------------------------------------------------------------------------- */
+
+struct termios original_tio;
+
+void disable_input_buffering() {
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key() {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
 
 /* -------------------------------------------------------------------------- */
 /*                                   MEMORY                                   */
@@ -138,7 +174,17 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
 /*                                    MAIN                                    */
 /* -------------------------------------------------------------------------- */
 
+void handle_interrupt(int signal) {
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
 int main(int argc, const char **argv) {
+    // Set up input buffering
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+;
     // Check number of args
     if (argc < 2) {
         fprintf(stderr, "Usage: %s IMAGE_FILE [IMAGE_FILE]...\n", argv[0]);
@@ -358,5 +404,6 @@ int main(int argc, const char **argv) {
         }
     }
 
+    restore_input_buffering();
     return 0;
 }
